1059e3b1daf8f6925c5fa77f91f24095331e69eb
yoav <yoavdan.m@gmail.com>
2023-02-07 15:08:28 +0200

    basic game setup

--- src/character/index.ts
+++ src/character/index.ts
@@ -8,17 +8,17 @@ interface ICharacter {
 }
 
 export default class Character {
-  playerNum: 0 | 1;
-  ctx: CanvasRenderingContext2D;
-  name: string;
-  pos: TCoordinates;
-  hitbox: THitBox;
+  private playerNum: 0 | 1;
+  private ctx: CanvasRenderingContext2D;
+  private name: string;
+  private pos: TCoordinates;
+  private hitbox: THitBox;
 
   constructor({ game, name, playerNum }: ICharacter) {
     this.playerNum = playerNum;
     this.ctx = game.ctx;
-    this.name = name;
-    this.hitbox = { width: 60, height: 180 };
+    this.name = name; // this will become an extemtion on custom char
+    this.hitbox = { width: 60, height: 180 }; //same here is will become custom variables
     this.pos = this.getInitialPos();
   }
 
@@ -26,17 +26,19 @@ export default class Character {
     const btm = this.ctx.canvas.height;
     const { width: charW, height: charH } = this.hitbox;
     let xPos: number;
-    let yPos: number;
+    let yPos = btm - charH;
 
     if (this.playerNum === 0) {
-      //   xPos = this.ctx.canvas.width / 2 - charW * 2 - charW / 2;
       xPos = this.ctx.canvas.width / 2 - charW * 3;
-      console.log(btm);
-      return { x: xPos, y: btm - charH };
+      return { x: xPos, y: yPos };
     } else {
       xPos = this.ctx.canvas.width / 2 + charW * 2;
-      return { x: xPos, y: btm - charH };
+      return { x: xPos, y: yPos };
     }
+  }
+
+  private registerControls(): void {
+    // TODO: initialzing the player controls
   }
 
   draw() {
@@ -45,10 +47,11 @@ export default class Character {
     const { x, y } = this.pos;
 
     this.ctx.fillStyle = 'red';
+    this.ctx.fillRect(x, y, charW, charH);
+    // for debug
     this.ctx.moveTo(w / 2, 0);
     this.ctx.lineTo(w / 2, h);
     this.ctx.strokeStyle = 'white';
     this.ctx.stroke();
-    this.ctx.fillRect(x, y, charW, charH);
   }
 }
