f629fd2e165c7e2309edea746beefbb906eb151f
yoav <yoavdan.m@gmail.com>
2023-02-11 19:33:42 +0200

    collide fighters

--- src/fighter/player2.ts
+++ src/fighter/player2.ts
@@ -36,7 +36,7 @@ export default class Fighter1 extends Fighter {
         pressed: false,
       },
     };
-    this.color = 'green';
+    this.color = 'yellow';
     this.startPos();
     this.regeisterControls();
     this.obstacle.pos = this.pos;
