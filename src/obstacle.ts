bd4f3237bf63031a4e6e80ac3fd32f43ec770243
yoav <yoavdan.m@gmail.com>
2023-02-11 19:23:20 +0200

    fighters collide is pretty much done

--- src/obstacle.ts
+++ src/obstacle.ts
@@ -42,22 +42,44 @@ export default class Obstacle {
       p1.pos.y < p2.pos.y + p2.hitbox.height &&
       p1.hitbox.height + p1.pos.y > p2.pos.y
     ) {
+      //player1 in on left
       if (p1.pos.x < p2.pos.x) {
         if (p1.keys?.ArrowRight.pressed && p2.keys?.a.pressed) {
           const temp = p1.pos.x;
           p1.pos.x = p2.pos.x - p1.hitbox.width;
           p2.pos.x = temp + p1.hitbox.width;
         } else if (p1.keys?.ArrowRight.pressed && !p2.keys?.a.pressed) {
-          p2.pos.x = p1.pos.x + p1.hitbox.width;
+          //when on border
+          if (p2.pos.x + p2.hitbox.width >= this.game.ctx.canvas.width) {
+            p1.pos.x = p2.pos.x - p1.hitbox.width;
+          } else {
+            p2.pos.x = p1.pos.x + p1.hitbox.width;
+          }
         } else if (p2.keys?.a.pressed && !p1.keys?.ArrowLeft.pressed) {
-          p1.pos.x = p2.pos.x - p1.hitbox.width;
+          if (p2.pos.x < p2.hitbox.width) {
+            p2.pos.x = p2.hitbox.width;
+          } else {
+            p1.pos.x = p2.pos.x - p1.hitbox.width;
+          }
         }
+        //player2 in on left
       } else {
-        if (p1.keys?.ArrowLeft.pressed && !p2.keys?.d.pressed) {
+        if (p1.keys?.ArrowLeft.pressed && p2.keys?.d.pressed) {
+          const temp = p2.pos.x;
           p2.pos.x = p1.pos.x - p2.hitbox.width;
-        }
-        if (p2.keys?.d.pressed && !p1.keys?.ArrowRight.pressed) {
-          p1.pos.x = p2.pos.x + p2.hitbox.width;
+          p1.pos.x = temp + p2.hitbox.width;
+        } else if (p1.keys?.ArrowLeft.pressed && !p2.keys?.d.pressed) {
+          if (p1.pos.x < p2.hitbox.width) {
+            p1.pos.x = p2.hitbox.width;
+          } else {
+            p2.pos.x = p1.pos.x - p2.hitbox.width;
+          }
+        } else if (p2.keys?.d.pressed && !p1.keys?.ArrowRight.pressed) {
+          if (p1.pos.x + p1.hitbox.width >= this.game.ctx.canvas.width) {
+            p2.pos.x = p1.pos.x - p2.hitbox.width;
+          } else {
+            p1.pos.x = p2.pos.x + p2.hitbox.width;
+          }
         }
       }
     }
